e7b441afa450e346ccc43f167d6081b96733c808
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2023-04-13 19:30:40 -0700

    Add files via upload
    Updated the image files to look more like the default image files

---
+++ story1.ts
@@ -0,0 +1,19 @@
+namespace sprites.background {
+
+     //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const page8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const page2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const page1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const page4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const page3 = image.ofBuffer(hex``);
+
+}
